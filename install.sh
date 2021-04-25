d2ea0cffba1eae97cb270bf3d512ae7b0fa59272
A l - A l a m y <59588007+AL-AlamySploit@users.noreply.github.com>
2021-04-25 17:23:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,22 @@
+#Figlet
+#figlet -f slant "your text" | lolcat
+#figlet -f fontname "your text" | lolcat
+#figlet -f bloody "your text" -w 200000
+#Lolcat
+#echo "your text " | lolcat -a -d 50
+#Toilet
+#toilet 'your text'
+#toilet -f smblock 'your text'
+#toilet -f smblock --filter metal 'your text'
+#toilet -f smblock --filter border:metal 'your text'
+#toilet -f smblock --filter border:metal 'Welcome to the colored toilet!' --export svg > test.svg
+# linuxlogo -a -l | toilet --metal -f term
+#toilet -f ivrit 'Linux is fun!' | boxes -d cat -a hc -p h8 | lolcat
+#Cowsay
+#fortune | cowsay | toilet --metal -f term
+#cowsay -f stegosaurus "your text"
+#Gedit  file
+#gedit /etc/bash.bashrc
 A7Y='\033[01;32m'
 echo
 echo "${A7Y} █████╗ ██╗       █████╗ ██╗      █████╗ ███╗   ███╗██╗   ██╗";
