df3c64b0e794340862cb557525def05946f56b2c
A l - A l a m y <59588007+AL-AlamySploit@users.noreply.github.com>
2021-04-25 16:15:46 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+A7Y='\033[01;32m'
+echo
+echo "${A7Y} █████╗ ██╗       █████╗ ██╗      █████╗ ███╗   ███╗██╗   ██╗";
+echo "${A7Y}██╔══██╗██║      ██╔══██╗██║     ██╔══██╗████╗ ████║╚██╗ ██╔╝";
+echo "${A7Y}███████║██║█████╗███████║██║     ███████║██╔████╔██║ ╚████╔╝";
+echo "${A7Y}██╔══██║██║╚════╝██╔══██║██║     ██╔══██║██║╚██╔╝██║  ╚██╔╝";
+echo "${A7Y}██║  ██║███████╗ ██║  ██║███████╗██║  ██║██║ ╚═╝ ██║   ██║";
+echo "${A7Y}╚═╝  ╚═╝╚══════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝   ╚═╝";
+echo
+printf "$A7Y[+] witing to Down pkg ..\n"
+sudo apt-get install figlet
+sudo apt-get install ruby
+gem install lolcat
+sudo apt-get install cmatrix
+sudo apt-get install toilet
+sudo apt-get install cowsay
+echo "$A7Y[+] Done..!"
